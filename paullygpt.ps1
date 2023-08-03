e02981d63651765e0936f541a146be58ef0af559
paullyvenne <paul.venne@gmail.com>
2023-08-03 19:37:13 -0400

    fx

--- paullygpt.ps1
+++ paullygpt.ps1
@@ -46,8 +46,27 @@ $cleanname = $ticksString.Replace(" ", "").Replace(".", "")
 $transcriptPath = ".\paullygpt\$cleanname.log.txt"
 Start-Transcript -Path $transcriptPath 
 
-#ASCII Art Wall
-ShowMatrix > $null
+$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.ForegroundColor = "Green"
+$matrixChars = @('█', '▓', '▒', '░', ' ')
+
+function ShowMatrix {
+    # dynamaically set to window width
+    $MAX = $Host.UI.RawUI.WindowSize.Width 
+
+    for ($j = 0; $j -lt 10; $j++) {
+        $randomChar = ""
+
+        for ($i = 0; $i -lt $MAX; $i++) {
+            $randomChar += $matrixChars | Get-Random
+        }
+
+        Write-Host -NoNewline $randomChar
+
+        # Add a small delay for the animation effect
+        Start-Sleep -Milliseconds 10
+    }
+}
 
 # Display Artificial Entity's Properties
 $aboutme = Get-CurrentAgent
